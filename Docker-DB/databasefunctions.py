52f11125db68a70a50f41832454e7f725f7a0895
Beauttah <wanjabeauttah@gmail.com>
2024-10-06 15:49:22 -0400

    updated the python script to connect to db and initialize schema, updated schema

--- Docker-DB/databasefunctions.py
+++ Docker-DB/databasefunctions.py
@@ -1,18 +1,7 @@
-
-import pyscopg2  
+import psycopg2
 
 conn = None
 
-def connect_db(dbname, dbuser, dbpass, dbhost) :
-    global conn
-    if conn is None or conn.closed:
-        conn = pyscopg2.connect(name = dbname, user = dbuser, password = dbpass, host = dbhost)
-    cur = conn.cursor()
-    conn_tuple = (conn, cur)
-    return conn_tuple
-   
-
-    
 """
 Function checks if a connection has already been established, if one hasn't been established then a new one is made.
 Once the connection is made, a cursor is made from that connection. The cursor allows the exectuion of statements. The 
@@ -20,3 +9,48 @@ using the cursor, various sql statements can be executed to modify the database.
 are returned as a tuple. Once done using the connection, both the cursor and the connection have to be closed using the 
 close method.
 """
+# EDIT by Beauttah -- We need error handling for this function
+def connect_db(dbname, user, password, host) :
+    global conn
+    if conn is None or conn.closed:
+        conn = psycopg2.connect(
+            dbname = dbname, 
+            user = user, 
+            password = password, 
+            host = host)
+    cur = conn.cursor()
+    conn_tuple = (conn, cur)
+    return conn_tuple
+
+
+# Modified by Beauttah to add a function that initializes the schema 
+conn_tuple = connect_db("pswdDB", "myuser","mypassword","localhost")
+conn,cur  = conn_tuple
+
+def create_passwords_table():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS passwords (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        url TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+    """
+    
+    #use the global variables from the connect_db function return values
+    cur.execute(create_table_query)
+    conn.commit()  # Save the changes to the database
+    print("Table 'passwords' created successfully.")    
+
+create_passwords_table()
+
+# Close the cursor object and connection
+cur.close()
+conn.close()
+
+
+'''
+CRUD Operations for SPRINT #3 will be implemented here
+'''
