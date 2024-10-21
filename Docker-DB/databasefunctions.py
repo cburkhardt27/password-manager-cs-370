c6e84e5dd228df8f50f591258e6dcc951442d302
Beauttah <wanjabeauttah@gmail.com>
2024-10-21 10:11:57 -0400

    all crud operations implemented

--- Docker-DB/databasefunctions.py
+++ Docker-DB/databasefunctions.py
@@ -1,5 +1,5 @@
 import psycopg2
-from encryption_functions import encode_new_password, decode_vault_password
+from Encryption.encryption_functions import encode_new_password, decode_vault_password
 
 conn = None
 
@@ -53,8 +53,10 @@ conn.close()
 
 
 '''
-CRUD Operations for SPRINT #3 will be implemented here
+CRUD Operations for SPRINT #3 & #4 will be implemented here
 '''
+
+# Add a new password entry to the vault. Params: Username, URL, Password
 def add_password_entry(username, url, plaintext_password):
     encrypted_password = encode_new_password(plaintext_password)
 
@@ -75,6 +77,7 @@ def add_password_entry(username, url, plaintext_password):
         conn.close()
 
 
+# Retrieve password from vault and decrypting it to plaintext. Params: Username, URL
 def get_password(username, url):
     query = """
     SELECT password FROM passwords
@@ -96,7 +99,8 @@ def get_password(username, url):
         cur.close()
         conn.close()
     
-
+# Delete password entry. Params: Username, URL
+# TODO -- Veryfying that the deletion is being done by actual user!!
 def delete_password(username, url):
     query ="""
     DELETE FROM passwords
@@ -113,3 +117,62 @@ def delete_password(username, url):
     finally:
         cur.close()
         conn.close()
+
+# Update old password with a new one. Params: Username, URL, New Password 
+def update_password_entry(username, url, new_plaintext_password):
+    new_encrypted_password = encode_new_password(new_plaintext_password)
+
+    query = """
+    UPDATE passwords
+    SET password = %s, updated_at = CURRENT_TIMESTAMP
+    WHERE username = %s AND url = %s;
+    """
+
+    try:
+        conn, cur = connect_db("pswdDB", "myuser", "mypassword", "localhost")
+        cur.execute(query, (new_encrypted_password, username, url))
+        conn.commit()
+        print("Password entry updated successfully.")
+    except Exception as e:
+        print(f"Error updating password entry: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+# Uodate username associated with a password. Params: Current Username, New Username, URL
+def update_username(current_username, new_username, url):
+    # First, validate that the password entry exists for the current username and URL
+    query = """
+    SELECT password FROM passwords
+    WHERE username = %s AND url = %s;
+    """
+    update_query = """
+    UPDATE passwords
+    SET username = %s, updated_at = CURRENT_TIMESTAMP
+    WHERE username = %s AND url = %s;
+    """
+
+    try:
+        conn, cur = connect_db("pswdDB", "myuser", "mypassword", "localhost")
+        cur.execute(query, (current_username, url))
+        result = cur.fetchone()
+        
+        if result:
+            cur.execute(update_query, (new_username, current_username, url))
+            conn.commit()
+            print("Username updated successfully.")
+        else:
+            print("No matching entry found for the current username and URL.")
+    except Exception as e:
+        print(f"Error updating username: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+'''
+QUESTIONS:
+- Do we need to verify password for CRUD operations or is the masterpassword 
+used for a one-time-is-enough verification??
+- Re-evaluate security of the design of DB functions...
+'''
