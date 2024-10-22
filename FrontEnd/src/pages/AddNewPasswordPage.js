03466c2600882b84d82f09802db0623641315003
Michi Okahata <michi.okahata@outlook.com>
2024-10-21 20:37:14 -0400

    Integrated password strength meter into the password textfield.

--- FrontEnd/src/pages/AddNewPasswordPage.js
+++ FrontEnd/src/pages/AddNewPasswordPage.js
@@ -93,6 +93,44 @@ const AddNewPasswordPage = () => {
     navigate('/HomePageNoPasswords');
   };
 
+  // Password meter parts
+  // Security?
+  const [password, setPassword] = useState('');
+  const [strength, setStrength] = useState(0);
+  const [recommend, setRecommend] = useState('Recommendations');
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    checkStrength(event.target.value);
+    checkRecommend(event.target.value);
+    handleChange(event);
+  };
+
+  const checkLength = (password) => password.length >= 8;
+  const checkUppercase = (password) => /[A-Z]/.test(password);
+  const checkLowercase = (password) => /[a-z]/.test(password);
+  const checkNumbers = (password) => /\d/.test(password);
+  const checkSpecialChars = (password) => /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  const checkStrength = (password) => {
+    let s = 0;
+    if (checkLength(password)) s += 1;
+    if (checkUppercase(password)) s += 1;
+    if (checkLowercase(password)) s += 1;
+    if (checkNumbers(password)) s += 1;
+    if (checkSpecialChars(password)) s += 1;
+    setStrength(s / 6);
+  };
+
+  const checkRecommend = (password) => {
+    if (!checkLength(password)) setRecommend('At least 8 characters');
+    else if (!checkUppercase(password)) setRecommend('Add uppercase');
+    else if (!checkLowercase(password)) setRecommend('Add lowercase');
+    else if (!checkNumbers(password)) setRecommend('Add numbers');
+    else if (!checkSpecialChars(password)) setRecommend('Add special characters');
+    else setRecommend('Secure!')
+  };
+
   return (
     <GradientBackground>
       {/* Sidebar Navigation */}
@@ -182,11 +220,24 @@ const AddNewPasswordPage = () => {
             <TextField
               label="Password"
               name="password"
+              type="password"
               value={formData.password}
-              onChange={handleChange}
+              onChange={handlePasswordChange}
               variant="outlined"
               fullWidth
-              sx={{ backgroundColor: '#B39DDB', borderRadius: '15px' }}
+              sx={{ 
+                background: (strength == 0) ? 
+                    '#B39DDB':
+                    `linear-gradient(to right, #5883F2 ${strength * 100 - 20}%, #B39DDB ${strength * 100 + 20}%)`,
+                borderRadius: '15px',
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                      <span>{(strength == 0) ? '' : recommend}</span>
+                  </InputAdornment>
+                )
+              }}
             />
             <TextField
               label="Note"
