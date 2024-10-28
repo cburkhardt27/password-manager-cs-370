3c91cad1c88b1bc991c4944509b6c1cce9bb8c11
Beauttah <wanjabeauttah@gmail.com>
2024-10-28 09:46:42 -0400

    added main file for testing and changes to compose to work with backend stuff

---
+++ DockerDB/_init__.py
@@ -0,0 +1,12 @@
+# Importing database functions to make them accessible directly from the package
+'''
+from .databasefunctions import (
+    connect_db,
+    create_passwords_table,
+    add_password_entry,
+    get_password,
+    update_password_entry,
+    delete_password,
+    update_username
+)
+'''
